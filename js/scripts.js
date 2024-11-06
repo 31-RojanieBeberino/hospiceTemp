9431d219b0d11851fc10d46779cb084b8b1baf0b
Roj Beberino <rojaniebeberino0206@gmail.com>
2024-11-06 17:27:12 +0800

    done with the pages and mobile responsive.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
     $('#mselect').chosen();
+
+    $('#datepicker').datepicker();
+
+    $('#datepicker').on('changeDate', function() {
+        $('#my_hidden_input').val(
+            $('#datepicker').datepicker('getFormattedDate')
+        );
+    });
+
+
+    $('#listCard').click(function(){
+        $('.listCards').css('display', 'flex');
+        $('.grdResult').css('display', 'none');
+        $('.rsltMap').css('display', 'none');
+    });
+
+    $('#grdCard').click(function(){
+        $('.listCards').css('display', 'none');
+        $('.grdResult').css('display', 'flex');
+        $('.rsltMap').css('display', 'none');
+    });
+
+    $('#rslt_map').click(function(){
+        $('.listCards').css('display', 'none');
+        $('.grdResult').css('display', 'none');
+        $('.rsltMap').css('display', 'block');
+    });
+
+
 });
 
 
